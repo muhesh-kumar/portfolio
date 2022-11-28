906dab71754a07fd5727db92d91081ce15ec19a6
Muhesh Kumar <muhesh4102002@gmail.com>
2022-11-28 23:19:59 +0530

    feat: migrate home page to astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,19 @@
-import { defineConfig } from "astro/config";
+import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-import react from "@astrojs/react";
+import react from '@astrojs/react';
 
 // https://astro.build/config
-import tailwind from "@astrojs/tailwind";
+import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), tailwind()],
+  integrations: [
+    react(),
+    tailwind({
+      // Example: Disable injecting a basic `base.css` import on every page.
+      // Useful if you need to define and/or import your own custom `base.css`.
+      // config: { applyBaseStyles: false },
+    }),
+  ],
 });
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        spaceGrotesk: ['Space Grotesk', 'sans-serif'],
+        yellowTail: ['Yellowtail', 'cursive'],
+      },
+      colors: {
+        blue: '#188aec',
+        fontColor: '#4b5563',
+      },
+    },
   },
   plugins: [],
 };
