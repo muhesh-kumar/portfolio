4001c0b249a803aa3250afb71133d2524d809c27
Muhesh Kumar <muhesh4102002@gmail.com>
2022-11-29 12:12:05 +0530

    feat: give equal width to project card children

--- astro.config.mjs
+++ astro.config.mjs
@@ -10,10 +10,6 @@ import tailwind from '@astrojs/tailwind';
 export default defineConfig({
   integrations: [
     react(),
-    tailwind({
-      // Example: Disable injecting a basic `base.css` import on every page.
-      // Useful if you need to define and/or import your own custom `base.css`.
-      // config: { applyBaseStyles: false },
-    }),
+    tailwind(),
   ],
 });
