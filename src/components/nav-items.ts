9e8da4b0d9a9e0985af060e41b085a7dfc53f0fe
Muhesh Kumar <muhesh4102002@gmail.com>
2023-10-02 18:19:54 +0530

    feat: add tooltips to bottombar icons

--- src/components/nav-items.ts
+++ src/components/nav-items.ts
@@ -11,25 +11,31 @@ export default [
   {
     icon: faHouse,
     route: '/',
+    pageName: 'Home',
   },
   {
     icon: faUser,
     route: '/about',
+    pageName: 'About',
   },
   {
     icon: faLightbulb,
     route: '/projects',
+    pageName: 'Projects',
   },
   {
     icon: faToolbox,
     route: '/skills',
+    pageName: 'Skills',
   },
   {
     icon: faRoad,
     route: '/experience',
+    pageName: 'Experience',
   },
   {
     icon: faTrophy,
     route: '/achievements',
+    pageName: 'Achievements',
   },
 ];
